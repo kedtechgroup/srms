b95556b8fe454de51e8c3632b9b3db37c8640f48
jstoreinthehills <salimjjuma@gmail.com>
2020-02-04 11:23:28 +0300

    Added Error Exception Handlers

--- add-subjectcombination.php
+++ add-subjectcombination.php
@@ -1,70 +1,83 @@
 <?php
+
 session_start();
 error_reporting(0);
-include('includes/config.php');
-if(strlen($_SESSION['alogin'])=="")
-    {
-    header("Location: index.php");
-    }
-    else{
-if(isset($_POST['submit']))
-{
-$class=$_POST['class'];
-$subject=$_POST['subject'];
-$status=1;
-$sql="INSERT INTO  tblsubjectcombination(ClassId,SubjectId,status) VALUES(:class,:subject,:status)";
-$query = $dbh->prepare($sql);
-$query->bindParam(':class',$class,PDO::PARAM_STR);
-$query->bindParam(':subject',$subject,PDO::PARAM_STR);
-$query->bindParam(':status',$status,PDO::PARAM_STR);
-$query->execute();
-$lastInsertId = $dbh->lastInsertId();
-if($lastInsertId)
-{
-$msg="Combination added successfully";
-}
-else
-{
-$error="Something went wrong. Please try again";
-}
 
-}
+include('includes/config.php');
+include('includes/functions.php');
+
+if (strlen($_SESSION['alogin']) == "") {
+
+    redirect_To("index.php");
+} else {
+
+    if (isset($_POST['submit'])) {
+
+        $class = $_POST['class'];
+        $subject = $_POST['subject'];
+
+        // Adding status to class.
+        $status = 1;
+
+        $sql = "INSERT INTO  tblsubjectcombination(ClassId,SubjectId,status)
+                VALUES(:class,:subject,:status)";
+
+        $query = $dbh->prepare($sql);
+
+        $query->bindParam(':class', $class, PDO::PARAM_STR);
+        $query->bindParam(':subject', $subject, PDO::PARAM_STR);
+        $query->bindParam(':status', $status, PDO::PARAM_STR);
+
+        $query->execute();
+
+        $lastInsertId = $dbh->lastInsertId();
+
+        if ($lastInsertId) {
+
+            $msg = "Combination added successfully";
+        } else {
+
+            $error = $dbh->errorInfo();
+        }
+    }
 ?>
-<!DOCTYPE html>
-<html lang="en">
+    <!DOCTYPE html>
+    <html lang="en">
+
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    	<meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>SMS Admin Subject Combination< </title>
-        <link rel="stylesheet" href="css/bootstrap.min.css" media="screen" >
-        <link rel="stylesheet" href="css/font-awesome.min.css" media="screen" >
-        <link rel="stylesheet" href="css/animate-css/animate.min.css" media="screen" >
-        <link rel="stylesheet" href="css/lobipanel/lobipanel.min.css" media="screen" >
-        <link rel="stylesheet" href="css/prism/prism.css" media="screen" >
-        <link rel="stylesheet" href="css/select2/select2.min.css" >
-        <link rel="stylesheet" href="css/main.css" media="screen" >
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>SRMS Add Subject to Class</title>
+        <link rel="stylesheet" href="css/bootstrap.min.css" media="screen">
+        <link rel="stylesheet" href="css/font-awesome.min.css" media="screen">
+        <link rel="stylesheet" href="css/animate-css/animate.min.css" media="screen">
+        <link rel="stylesheet" href="css/lobipanel/lobipanel.min.css" media="screen">
+        <link rel="stylesheet" href="css/prism/prism.css" media="screen">
+        <link rel="stylesheet" href="css/select2/select2.min.css">
+        <link rel="stylesheet" href="css/main.css" media="screen">
         <script src="js/modernizr/modernizr.min.js"></script>
     </head>
+
     <body class="top-navbar-fixed">
         <div class="main-wrapper">
 
             <!-- ========== TOP NAVBAR ========== -->
-  <?php include('includes/topbar.php');?>
+            <?php include('includes/topbar.php'); ?>
             <!-- ========== WRAPPER FOR BOTH SIDEBARS & MAIN CONTENT ========== -->
             <div class="content-wrapper">
                 <div class="content-container">
 
                     <!-- ========== LEFT SIDEBAR ========== -->
-                   <?php include('includes/leftbar.php');?>
+                    <?php include('includes/leftbar.php'); ?>
                     <!-- /.left-sidebar -->
 
                     <div class="main-page">
 
-                     <div class="container-fluid">
+                        <div class="container-fluid">
                             <div class="row page-title-div">
                                 <div class="col-md-6">
-                                    <h2 class="title">Add Subject Combination</h2>
+                                    <h2 class="title">Add Subject To Class</h2>
 
                                 </div>
 
@@ -75,113 +88,133 @@ $error="Something went wrong. Please try again";
                                 <div class="col-md-6">
                                     <ul class="breadcrumb">
                                         <li><a href="dashboard.php"><i class="fa fa-home"></i> Home</a></li>
-                                        <li> Subjects</li>
-                                        <li class="active">Add Subject Combination</li>
+                                        <li> <a href="manage-subjects.php"> Subjects</a></li>
+                                        <li class="active">Add Class Subject Combination</li>
                                     </ul>
                                 </div>
 
                             </div>
                             <!-- /.row -->
                         </div>
+                        <br>
                         <div class="container-fluid">
 
-                        <div class="row">
-                                    <div class="col-md-12">
-                                        <div class="panel">
-                                            <div class="panel-heading">
-                                                <div class="panel-title">
-                                                    <h5>Add Subject Combination</h5>
-                                                </div>
-                                            </div>
-                                            <div class="panel-body">
-                                            <?php if($msg){?>
-                                            <div class="alert alert-success left-icon-alert" role="alert">
-                                             <strong>Well done!</strong><?php echo htmlentities($msg); ?>
-                                             </div><?php }
-                                            else if($error){?>
-                                                <div class="alert alert-danger left-icon-alert" role="alert">
-                                                                                        <strong>Oh snap!</strong> <?php echo htmlentities($error); ?>
-                                                                                    </div>
-                                        <?php } ?>
-
-                                                <form class="form-horizontal" method="post">
-                                                    <div class="form-group">
-                                                        <label for="default" class="col-sm-2 control-label">Class</label>
-                                                        <div class="col-sm-10">
-                                                     <select name="class" class="form-control" id="default" required="required">
-                                                    <option value="">Select Class</option>
-                                                    <?php $sql = "SELECT * from tblclasses";
-                                                    $query = $dbh->prepare($sql);
-                                                    $query->execute();
-                                                    $results=$query->fetchAll(PDO::FETCH_OBJ);
-                                                    if($query->rowCount() > 0)
-                                                    {
-                                                    foreach($results as $result)
-                                                    {   ?>
-                                                    <option value="<?php echo htmlentities($result->id); ?>"><?php echo htmlentities($result->ClassName); ?>&nbsp; Section-<?php echo htmlentities($result->Section); ?></option>
-                                                    <?php }} ?>
-                                                     </select>
-                                                                                                            </div>
-                                                                                                        </div>
-                                                    <div class="form-group">
-                                                        <label for="default" class="col-sm-2 control-label">Subject</label>
-                                                        <div class="col-sm-10">
-                                                     <select name="subject" class="form-control" id="default" required="required">
-                                                    <option value="">Select Subject</option>
-                                                    <?php $sql = "SELECT * from tblsubjects";
-                                                    $query = $dbh->prepare($sql);
-                                                    $query->execute();
-                                                    $results=$query->fetchAll(PDO::FETCH_OBJ);
-                                                    if($query->rowCount() > 0)
-                                                    {
-                                                    foreach($results as $result)
-                                                    {   ?>
-                                                    <option value="<?php echo htmlentities($result->id); ?>"><?php echo htmlentities($result->SubjectName); ?></option>
-                                                    <?php }} ?>
-                                                     </select>
-                                                        </div>
-                                                    </div>
-
-
-
-                                                    <div class="form-group">
-                                                        <div class="col-sm-offset-2 col-sm-10">
-                                                            <button type="submit" name="submit" class="btn btn-primary">Add</button>
-                                                        </div>
-                                                    </div>
-                                                </form>
-
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="panel">
+                                        <div class="panel-heading">
+                                            <div class="panel-title">
+                                                <h5>Add Subject To Class</h5>
                                             </div>
                                         </div>
+                                        <div class="panel-body">
+                                            <?php if ($msg) { ?>
+                                                <div class="alert alert-success left-icon-alert" role="alert">
+                                                    <strong>Well done!</strong><?php echo htmlentities($msg); ?>
+                                                    <button type="button" class="close" data-dismiss="alert" aria-label="close">
+                                                        <span aria-hidden="true"> &times; </span>
+                                                    </button>
+                                                </div><?php } else if ($error) { ?>
+                                                <div class="alert alert-danger left-icon-alert" role="alert">
+                                                    <strong>Oh snap!</strong> <?php echo htmlentities($error); ?>
+                                                    <button type="button" class="close" data-dismiss="alert" aria-label="close">
+                                                        <span aria-hidden="true"> &times; </span>
+                                                    </button>
+                                                </div>
+                                            <?php } ?>
+
+                                            <form class="form-horizontal" method="post">
+
+                                                <div class="form-group">
+                                                    <label for="default" class="col-sm-2 control-label">Class</label>
+                                                    <div class="col-sm-10">
+                                                        <select name="class" class="form-control" id="default" required="required">
+                                                            <option value="">Select Class</option>
+
+                                                            <?php
+                                                            $sql = "SELECT * from tblclasses";
+                                                            $query = $dbh->prepare($sql);
+                                                            $query->execute();
+                                                            $results = $query->fetchAll(PDO::FETCH_OBJ);
+                                                            if ($query->rowCount() > 0) {
+                                                                foreach ($results as $result) {   ?>
+                                                                    <option value="<?php echo htmlentities($result->id); ?>"><?php echo htmlentities($result->ClassName); ?>&nbsp; Section-<?php echo htmlentities($result->Section); ?></option>
+                                                            <?php }
+                                                            } ?>
+                                                        </select>
+                                                    </div>
+                                                </div>
+
+                                                <div class="form-group">
+                                                    <label for="default" class="col-sm-2 control-label">Subject</label>
+                                                    <div class="col-sm-10">
+
+                                                        <select name="subject" class="form-control" id="default" required="required">
+
+                                                            <option value="">Select Subject</option>
+
+                                                            <?php
+                                                            $sql = "SELECT * from tblsubjects";
+
+                                                            $query = $dbh->prepare($sql);
+
+                                                            $query->execute();
+
+                                                            $results = $query->fetchAll(PDO::FETCH_OBJ);
+
+                                                            if ($query->rowCount() > 0) {
+                                                                foreach ($results as $result) {   ?>
+                                                                    <option value="<?php echo htmlentities($result->subject_id); ?>"><?php echo htmlentities($result->SubjectName); ?></option>
+                                                            <?php }
+                                                            } ?>
+                                                        </select>
+                                                    </div>
+                                                </div>
+
+                                                <div class="form-group">
+                                                    <div class="col-sm-offset-2 col-sm-10">
+                                                        <button type="submit" name="submit" class="btn btn-primary">Add</button>
+
+                                                    </div>
+                                                </div>
+
+                                            </form>
+
+                                        </div>
                                     </div>
-                                    <!-- /.col-md-12 -->
                                 </div>
+                                <!-- /.col-md-12 -->
+                            </div>
+                        </div>
                     </div>
+                    <!-- /.content-container -->
                 </div>
-                <!-- /.content-container -->
+                <!-- /.content-wrapper -->
             </div>
-            <!-- /.content-wrapper -->
-        </div>
-        <!-- /.main-wrapper -->
-        <script src="js/jquery/jquery-2.2.4.min.js"></script>
-        <script src="js/bootstrap/bootstrap.min.js"></script>
-        <script src="js/pace/pace.min.js"></script>
-        <script src="js/lobipanel/lobipanel.min.js"></script>
-        <script src="js/iscroll/iscroll.js"></script>
-        <script src="js/prism/prism.js"></script>
-        <script src="js/select2/select2.min.js"></script>
-        <script src="js/main.js"></script>
-        <script>
-            $(function($) {
-                $(".js-states").select2();
-                $(".js-states-limit").select2({
-                    maximumSelectionLength: 2
+            <!-- /.main-wrapper -->
+            <script src="js/jquery/jquery-2.2.4.min.js"></script>
+            <script src="js/bootstrap/bootstrap.min.js"></script>
+            <script src="js/pace/pace.min.js"></script>
+            <script src="js/lobipanel/lobipanel.min.js"></script>
+            <script src="js/iscroll/iscroll.js"></script>
+            <script src="js/prism/prism.js"></script>
+            <script src="js/select2/select2.min.js"></script>
+            <script src="js/main.js"></script>
+            <script>
+                if (window.history.replaceState) {
+                    window.history.replaceState(null, null, window.location.href);
+                }
+                $(function($) {
+                    $(".js-states").select2();
+                    $(".js-states-limit").select2({
+                        maximumSelectionLength: 2
+                    });
+                    $(".js-states-hide").select2({
+                        minimumResultsForSearch: Infinity
+                    });
                 });
-                $(".js-states-hide").select2({
-                    minimumResultsForSearch: Infinity
-                });
-            });
-        </script>
+            </script>
     </body>
-</html>
-<?PHP } ?>
+
+    </html>
+<?php } ?>
